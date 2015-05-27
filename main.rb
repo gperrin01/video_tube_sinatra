612015bf0c7f678c3163cda069f8c6e904dda7c5
Guillaume Perrin <gperrin01@gmail.com>
2015-05-27 20:16:15 +0100

    first upload working

---
+++ main.rb
@@ -0,0 +1,67 @@
+require 'pry'
+require 'pg'
+require 'sinatra'
+require 'sinatra/reloader' if development? 
+
+# Create a list of videos. Resources are videos
+#   1) Main page ('/videos') shows title and video frame, 
+#   GET all items on '/videos', and redirect '/' to /videos
+#   2) Click Upload to send me to page where can upload a new video  
+#   GET '/videos/upload' to give the form 
+#   3) Submit a new video with the form
+#   POST on '/videos'
+#   4) click on title to SHOW specific video page
+#   GET on '/videos/:id'
+#   ONCE SHOWN WE CAN DO EDIT OR DELETE FROM THE SHOW PAGE
+  #   5) Edit a video
+  #   send me to the form for editing: GET '/videos/:id/edit'
+  #   6) Update the video
+  #   POST 'videos'/:id
+  #   7) Delete an item: click the button delete
+  #   DELETE on '/videos/:id'
+
+
+get '/' do 
+  redirect to ('/videos')
+end
+get '/videos' do 
+  sql = "SELECT * FROM videos"
+  @videos = run_sql(sql)
+  erb :videos
+end
+
+get '/videos/upload' do
+  erb :upload
+end
+
+post '/videos' do
+  url_embed = format_for_embed(params['url'])
+  sql = "INSERT INTO videos (title, description, url, url_embed, genre) values ('#{params['title']}', '#{params['description']}', '#{params['url']}', '#{url_embed}', '#{params['genre']}' )"
+  binding.pry
+  run_sql(sql)
+  redirect to ('/videos')
+end
+
+
+
+
+def format_for_embed(url)
+  url.gsub('watch?v=', 'embed/')
+end
+def sql_string(value)
+  "'#{value.gsub("'","''")}'"
+end
+
+
+
+
+private
+
+def run_sql(sql)
+  connect = PG.connect(dbname: 'videos', host: 'localhost')
+  begin
+    connect.exec(sql)
+  ensure
+    connect.close
+  end
+end
---
+++ videos_sql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE videos(
+  id serial8 primary key, 
+  title varchar(225),
+  description text,
+  url varchar(225),
+  url_embed varchar(225),
+  genre varchar(30)
+);
